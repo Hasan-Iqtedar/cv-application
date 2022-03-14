4d0e230fb2cb13f0f6ea76b1d5538a12acdb431f
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-03-14 19:35:56 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import GeneralInfo from "./components/GeneralInfo";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
