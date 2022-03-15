fc125b957342f970e62dcba4a89272f8539f6c52
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-03-15 09:40:11 +0500

    Handle state for GeneralInfo

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -6,57 +6,104 @@ import SubSection from "./SubSection";
 class GeneralInfo extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      title: "",
+      phoneNo: "",
+      email: "",
+      github: "",
+      linkedIn: "",
+    };
   }
+
+  updateFirstName = (e) => this.setState({ firstName: e.target.value });
+  updateLastName = (e) => this.setState({ lastName: e.target.value });
+  updateTitle = (e) => this.setState({ title: e.target.value });
+  updatePhoneNo = (e) => this.setState({ phoneNo: e.target.value });
+  updateEmail = (e) => this.setState({ email: e.target.value });
+  updateGithub = (e) => this.setState({ github: e.target.value });
+  updateLinkedIn = (e) => this.setState({ linkedIn: e.target.value });
 
   render() {
     return (
       <InfoSection title="General Information">
         <SubSection>
           <Container>
-            <label htmlFor="FirstName">First Name</label>
+            <label htmlFor="firstName">First Name</label>
             <input
               className="general-info-input"
-              id="FirstName"
+              id="firstName"
               type="text"
+              value={this.state.firstName}
+              onChange={this.updateFirstName}
             ></input>
           </Container>
 
           <Container>
-            <label htmlFor="FirstName">Last Name</label>
+            <label htmlFor="lastName">Last Name</label>
             <input
               className="general-info-input"
-              id="FirstName"
+              id="lastName"
               type="text"
+              value={this.state.lastName}
+              onChange={this.updateLastName}
             ></input>
           </Container>
 
           <Container>
-            <label>Title</label>
-            <input className="general-info-input"></input>
+            <label htmlFor="title">Title</label>
+            <input
+              className="general-info-input"
+              id="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.updateTitle}
+            ></input>
           </Container>
 
           <Container>
-            <label>Phone No.</label>
-            <input className="general-info-input"></input>
+            <label htmlFor="phone-no">Phone No.</label>
+            <input
+              className="general-info-input"
+              id="phone-no"
+              type="text"
+              value={this.state.phoneNo}
+              onChange={this.updatePhoneNo}
+            ></input>
           </Container>
 
           <Container>
-            <label>Email</label>
-            <input className="general-info-input"></input>
+            <label htmlFor="email">Email</label>
+            <input
+              className="general-info-input"
+              id="email"
+              value={this.state.email}
+              onChange={this.updateEmail}
+            ></input>
           </Container>
 
           <Container>
-            <label>Github</label>
-            <input className="general-info-input"></input>
+            <label htmlFor="github">Github</label>
+            <input
+              className="general-info-input"
+              id="github"
+              value={this.state.github}
+              onChange={this.updateGithub}
+            ></input>
           </Container>
 
           <Container>
-            <label>LinkedIn</label>
-            <input className="general-info-input"></input>
+            <label htmlFor="linkedIn">LinkedIn</label>
+            <input
+              className="general-info-input"
+              id="linkedIn"
+              value={this.state.linkedIn}
+              onChange={this.updateLinkedIn}
+            ></input>
           </Container>
-
         </SubSection>
-
       </InfoSection>
     );
   }
