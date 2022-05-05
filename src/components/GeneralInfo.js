e520762dd3e788703da225d400e2b8517e5b3fcc
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-05-05 18:08:37 +0500

    Update GeneralInfo component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,119 +1,118 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import Container from "./Container";
 import InfoSection from "./InfoSection";
 import SubSection from "./SubSection";
 
-class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
+const GeneralInfo = (props) => {
+  const [firstName, setFirstName] = useState(props.generalInfo.firstName);
+  const [lastName, setLastName] = useState(props.generalInfo.lastName);
+  const [title, setTitle] = useState(props.generalInfo.title);
+  const [phoneNo, setPhoneNo] = useState(props.generalInfo.phoneNo);
+  const [email, setEmail] = useState(props.generalInfo.email);
+  const [github, setGithub] = useState(props.generalInfo.github);
+  const [linkedIn, setLinkedIn] = useState(props.generalInfo.linkedIn);
 
-    this.state = {
-      firstName: props.generalInfo.firstName,
-      lastName: props.generalInfo.lastName,
-      title: props.generalInfo.title,
-      phoneNo: props.generalInfo.phoneNo,
-      email: props.generalInfo.email,
-      github: props.generalInfo.github,
-      linkedIn: props.generalInfo.linkedIn,
-    };
-  }
+  const updateFirstName = (e) => setFirstName(e.target.value);
+  const updateLastName = (e) => setLastName(e.target.value);
+  const updateTitle = (e) => setTitle(e.target.value);
+  const updatePhoneNo = (e) => setPhoneNo(e.target.value);
+  const updateEmail = (e) => setEmail(e.target.value);
+  const updateGithub = (e) => setGithub(e.target.value);
+  const updateLinkedIn = (e) => setLinkedIn(e.target.value);
 
-  updateFirstName = (e) => this.setState({ firstName: e.target.value });
-  updateLastName = (e) => this.setState({ lastName: e.target.value });
-  updateTitle = (e) => this.setState({ title: e.target.value });
-  updatePhoneNo = (e) => this.setState({ phoneNo: e.target.value });
-  updateEmail = (e) => this.setState({ email: e.target.value });
-  updateGithub = (e) => this.setState({ github: e.target.value });
-  updateLinkedIn = (e) => this.setState({ linkedIn: e.target.value });
+  return (
+    <InfoSection title="General Information">
+      <SubSection>
+        <Container className="container">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            className="general-info-input"
+            id="firstName"
+            type="text"
+            value={firstName}
+            onChange={updateFirstName}
+          ></input>
+        </Container>
 
-  render() {
-    return (
-      <InfoSection title="General Information">
-        <SubSection>
-          <Container className="container">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              className="general-info-input"
-              id="firstName"
-              type="text"
-              value={this.state.firstName}
-              onChange={this.updateFirstName}
-            ></input>
-          </Container>
+        <Container className="container">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            className="general-info-input"
+            id="lastName"
+            type="text"
+            value={lastName}
+            onChange={updateLastName}
+          ></input>
+        </Container>
 
-          <Container className="container">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              className="general-info-input"
-              id="lastName"
-              type="text"
-              value={this.state.lastName}
-              onChange={this.updateLastName}
-            ></input>
-          </Container>
+        <Container className="container">
+          <label htmlFor="title">Title</label>
+          <input
+            className="general-info-input"
+            id="title"
+            type="text"
+            value={title}
+            onChange={updateTitle}
+          ></input>
+        </Container>
 
-          <Container className="container">
-            <label htmlFor="title">Title</label>
-            <input
-              className="general-info-input"
-              id="title"
-              type="text"
-              value={this.state.title}
-              onChange={this.updateTitle}
-            ></input>
-          </Container>
+        <Container className="container">
+          <label htmlFor="phone-no">Phone No.</label>
+          <input
+            className="general-info-input"
+            id="phone-no"
+            type="text"
+            value={phoneNo}
+            onChange={updatePhoneNo}
+          ></input>
+        </Container>
 
-          <Container className="container">
-            <label htmlFor="phone-no">Phone No.</label>
-            <input
-              className="general-info-input"
-              id="phone-no"
-              type="text"
-              value={this.state.phoneNo}
-              onChange={this.updatePhoneNo}
-            ></input>
-          </Container>
+        <Container className="container">
+          <label htmlFor="email">Email</label>
+          <input
+            className="general-info-input"
+            id="email"
+            value={email}
+            onChange={updateEmail}
+          ></input>
+        </Container>
 
-          <Container className="container">
-            <label htmlFor="email">Email</label>
-            <input
-              className="general-info-input"
-              id="email"
-              value={this.state.email}
-              onChange={this.updateEmail}
-            ></input>
-          </Container>
+        <Container className="container">
+          <label htmlFor="github">Github</label>
+          <input
+            className="general-info-input"
+            id="github"
+            value={github}
+            onChange={updateGithub}
+          ></input>
+        </Container>
 
-          <Container className="container">
-            <label htmlFor="github">Github</label>
-            <input
-              className="general-info-input"
-              id="github"
-              value={this.state.github}
-              onChange={this.updateGithub}
-            ></input>
-          </Container>
-
-          <Container className="container">
-            <label htmlFor="linkedIn">LinkedIn</label>
-            <input
-              className="general-info-input"
-              id="linkedIn"
-              value={this.state.linkedIn}
-              onChange={this.updateLinkedIn}
-            ></input>
-          </Container>
-
-          <button
-            className="add-button"
-            onClick={this.props.sendGeneralInfo.bind(this, this.state)}
-          >
-            Add
-          </button>
-        </SubSection>
-      </InfoSection>
-    );
-  }
-}
+        <Container className="container">
+          <label htmlFor="linkedIn">LinkedIn</label>
+          <input
+            className="general-info-input"
+            id="linkedIn"
+            value={linkedIn}
+            onChange={updateLinkedIn}
+          ></input>
+        </Container>
+        <button
+          className="add-button"
+          onClick={props.sendGeneralInfo.bind(this, {
+            firstName,
+            lastName,
+            title,
+            phoneNo,
+            email,
+            github,
+            linkedIn,
+          })}
+        >
+          Add
+        </button>
+      </SubSection>
+    </InfoSection>
+  );
+};
 
 export default GeneralInfo;
