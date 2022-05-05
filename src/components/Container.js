47dab04cab070c3dfca54da701888f52486a1d53
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-05-05 16:32:13 +0500

    Update Container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,7 @@
-import { Component } from "react";
 import "./../styles/main.css";
 
-class Container extends Component {
-  render() {
-    return <div className={this.props.className}>{this.props.children}</div>;
-  }
-}
+const Container = (props) => {
+  return <div className={props.className}>{props.children}</div>;
+};
 
 export default Container;
