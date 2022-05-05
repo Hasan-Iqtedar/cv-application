eafb640110c2aa78653d366449ad21778145fa62
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-05-05 17:56:26 +0500

    Update EducationInfo component

--- src/components/EducationInfo.js
+++ src/components/EducationInfo.js
@@ -1,124 +1,113 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import SubSection from "./SubSection";
 import Container from "./Container";
 
-class EducationInfo extends Component {
-  constructor(props) {
-    super(props);
+const EducationInfo = (props) => {
+  const [degree, setDegree] = useState(props.educationInfo.degree);
+  const [institution, setInstitution] = useState(
+    props.educationInfo.institution
+  );
+  const [cgpa, setCgpa] = useState(props.educationInfo.cgpa);
+  const [toDate, setToDate] = useState(props.educationInfo.to);
+  const [fromDate, setFromDate] = useState(props.educationInfo.from);
 
-    this.state = {
-      id: props.educationInfo.id,
-      degree: props.educationInfo.degree,
-      institution: props.educationInfo.institution,
-      cgpa: props.educationInfo.cgpa,
-      to: props.educationInfo.to,
-      from: props.educationInfo.from,
-    };
-  }
+  const updateDegree = (e) => setDegree(e.target.value);
+  const updateInstitution = (e) => setInstitution(e.target.value);
+  const updateCgpa = (e) => setCgpa(e.target.value);
+  const updateToDate = (e) => setToDate(e.target.value);
+  const updateFromDate = (e) => setFromDate(e.target.value);
 
-  updateDegree = (e) => this.setState({ degree: e.target.value });
-  updateInstitution = (e) => this.setState({ institution: e.target.value });
-  updateCgpa = (e) => this.setState({ cgpa: e.target.value });
-  updateToDate = (e) => this.setState({ to: e.target.value });
-  updateFromDate = (e) => this.setState({ from: e.target.value });
-
-  updateEducation = () => {
-    const { degree, institution, cgpa, to, from } = this.state;
-
+  const updateEducation = () => {
     if (
       degree !== "" &&
       institution !== "" &&
       cgpa !== "" &&
-      to !== "" &&
-      from !== ""
+      toDate !== "" &&
+      fromDate !== ""
     ) {
-      this.props.sendEducationInfo({
-        id: this.props.id,
+      props.sendEducationInfo({
+        id: props.id,
         degree: degree,
         institution: institution,
         cgpa: cgpa,
-        to: to,
-        from: from,
+        to: toDate,
+        from: fromDate,
       });
     }
   };
 
-  render() {
-    console.log("EducationalInfo Component\n____________________");
+  return (
+    <SubSection>
+      <Container className="container">
+        <label htmlFor="degree">Degree Title</label>
+        <input
+          className="general-info-input"
+          id="degree"
+          type="text"
+          value={degree}
+          onChange={updateDegree}
+        ></input>
+      </Container>
 
-    return (
-      <SubSection>
+      <Container className="container">
+        <label htmlFor="institution">Institution</label>
+        <input
+          className="general-info-input"
+          id="institution"
+          type="text"
+          value={institution}
+          onChange={updateInstitution}
+        ></input>
+      </Container>
+
+      <Container className="container">
+        <label htmlFor="cgpa">CGPA</label>
+        <input
+          className="general-info-input"
+          id="cgpa"
+          type="text"
+          value={cgpa}
+          onChange={updateCgpa}
+        ></input>
+      </Container>
+
+      <Container className="container date-container">
         <Container className="container">
-          <label htmlFor="degree">Degree Title</label>
+          <label htmlFor="to">To</label>
           <input
             className="general-info-input"
-            id="degree"
+            id="to"
             type="text"
-            value={this.state.degree}
-            onChange={this.updateDegree}
+            placeholder="Date or Present"
+            value={toDate}
+            onChange={updateToDate}
           ></input>
         </Container>
 
         <Container className="container">
-          <label htmlFor="institution">Institution</label>
+          <label htmlFor="from">From</label>
           <input
             className="general-info-input"
-            id="institution"
-            type="text"
-            value={this.state.institution}
-            onChange={this.updateInstitution}
+            id="from"
+            type="date"
+            value={fromDate}
+            onChange={updateFromDate}
           ></input>
         </Container>
+      </Container>
 
-        <Container className="container">
-          <label htmlFor="cgpa">CGPA</label>
-          <input
-            className="general-info-input"
-            id="cgpa"
-            type="text"
-            value={this.state.cgpa}
-            onChange={this.updateCgpa}
-          ></input>
-        </Container>
-
-        <Container className="container date-container">
-          <Container className="container">
-            <label htmlFor="to">To</label>
-            <input
-              className="general-info-input"
-              id="to"
-              type="text"
-              placeholder="Date or Present"
-              value={this.state.to}
-              onChange={this.updateToDate}
-            ></input>
-          </Container>
-
-          <Container className="container">
-            <label htmlFor="from">From</label>
-            <input
-              className="general-info-input"
-              id="from"
-              type="date"
-              value={this.state.from}
-              onChange={this.updateFromDate}
-            ></input>
-          </Container>
-        </Container>
-
-        <div className="button-container">
-          <button onClick={this.updateEducation}>Update</button>
-          <button
-            onClick={() => {
-              this.props.deleteEducation(this.props.id);
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      </SubSection>
-    );
-  }
-}
+      <div className="button-container">
+        <button onClick={updateEducation}>Update</button>
+        <button
+          onClick={() => {
+            props.deleteEducation(props.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </SubSection>
+  );
+};
 
 export default EducationInfo;
