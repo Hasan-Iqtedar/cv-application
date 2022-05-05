fd38f2e7eedc8e704653eae820dc71034e05cd85
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-05-05 18:11:17 +0500

    Update SubSection component

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -1,10 +1,8 @@
-import { Component } from "react";
+import React from "react";
 import "./../styles/main.css";
 
-class SubSection extends Component {
-  render() {
-    return <div className="sub-section">{this.props.children}</div>;
-  }
-}
+const SubSection = (props) => (
+  <div className="sub-section">{this.props.children}</div>
+);
 
 export default SubSection;
